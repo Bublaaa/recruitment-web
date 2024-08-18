0e6df49ba53bd22beb6908f6671e50acebfbb0ef
Bublaaa <yanuarkrisna7@gmail.com>
2024-08-18 16:04:30 +0700

    Add client, admin, and landing page

---
+++ resources/views/layouts/clientPage.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/flowbite@2.5.1/dist/flowbite.min.css" rel="stylesheet" />
+    @vite('resources/css/app.css')
+</head>
+
+<body>
+    <h1 class="text-xl font-bold">
+        Admin Page
+    </h1>
+    <script src="https://cdn.jsdelivr.net/npm/flowbite@2.5.1/dist/flowbite.min.js"></script>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/admin', function () {
+    return view('/layouts/adminPage');
+});
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('/layouts/landingPage');
 });
